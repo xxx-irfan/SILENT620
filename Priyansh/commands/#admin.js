afd8c2862a28a366264a4c8f4045e10336727edd
xxx-irfan <sehzadikhan632@gmail.com>
2024-07-09 05:25:13 +0530

    Update commands

---
+++ Priyansh/commands/#admin.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "admin",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "PRINCE SILENT", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("admin") ||
+     react.includes("Admin") || react.includes("ADMIN") || react.includes("BOSS") ||
+react.includes("boss") ||
+react.includes("Prince") ||     
+react.includes("prince")) {
+    var msg = {
+        body: `${name} 🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
+┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
+🌸✦✧✧✧✧✰🍒✬✿╭┳✪✪╤───────────➛➣ ★�🆁🅸🅽🅲🅴★🆂🅸🅻🅴🅽🆃)✪��🆈★  ✬✿╭┳✪✪╤───────────➛➣🤩🌿✰✧✧✧✧✦🌸
+┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛    
+🕊️🌸
+...
+✮┼💞┼✮
+☸🕊️━━•🌸•━━🕊️☸
+✮☸✮
+✮┼🍫┼✮
+☸🎀━━•🧸•━━🎀☸
+✮┼🦢┼✮
+✮☸✮
+☸🌈━━•🤍•━━🌈☸
+✮☸✮
+✮┼❄️┼✮🥀💐✿`,attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpeg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }

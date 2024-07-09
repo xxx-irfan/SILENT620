a3a4ef26b6e8aedb11dbc507569d16094bb1789d
xxx-irfan <sehzadikhan632@gmail.com>
2024-07-09 07:44:59 +0530

    Update commands

---
+++ Priyansh/commands/#owner.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "owner",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "PRINCE SILENT", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("OWNER") ||
+     react.includes("Oner") || react.includes("Owner") || react.includes("owner") ||
+react.includes("OWNER") ||
+react.includes("Sir") ||     
+react.includes("sir")) {
+    var msg = {
+        body: `${name} 🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
+┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
+🌸✦✧✧✧✧✰🍒✬✿╭┳✪✪╤───────────➛➣ ★�🆁🅸🅽🅲🅴★🆂🅸🅻🅴🅽🆃)✪��🆈★  ✬✿╭┳✪✪╤───────────➛➣🤩🌿✰✧✧✧✧✦🌸
+┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛  ✿`,attachment: fs.createReadStream(__dirname + `/noprefix/Profil.png`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }

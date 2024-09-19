81bc2ec68a30a905a3321c1dd2de8b01c53bb357
xxx-irfan <sehzadikhan632@gmail.com>
2024-09-19 17:31:35 +0530

    Update commands

---
+++ Priyansh/commands/#goiadmin.js
@@ -0,0 +1,43 @@
+module.exports.config = {
+
+  name: "goiadmin",
+
+  version: "1.0.0",
+
+  hasPermssion: 0,
+
+  credits: "fixed by Chand Tricker",
+
+  description: "Bot will reply tag admin ",
+
+  commandCategory: "Other",
+
+  usages: "",
+
+  cooldowns: 1
+
+};
+
+module.exports.handleEvent = function({ api, event }) {
+
+  if (event.senderID !== "61551474205816") {
+
+    var aid = ["100092750349098"];
+
+    for (const id of aid) {
+
+    if ( Object.keys(event.mentions) == id) {
+
+      var msg = [" PUT YOUR TEXT"];
+
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+
+    }
+
+    }}
+
+};
+
+module.exports.run = async function({}) {
+
+        }

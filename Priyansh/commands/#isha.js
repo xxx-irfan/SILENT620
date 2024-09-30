2311cab2f2542d597242254d70fc27eb8cecdf80
xxx-irfan <sehzadikhan632@gmail.com>
2024-09-30 18:05:16 +0530

    Update #isha.js

--- Priyansh/commands/#isha.js
+++ Priyansh/commands/#isha.js
@@ -19,8 +19,7 @@ react.includes("Isha") ||
 react.includes("ISHA") ||     
 react.includes("isha")) {
     var msg = {
-        body: `${name} 𝐌𝐄𝐑𝐄 𝐃𝐎𝐒𝐓𝐎😱 𝐈𝐒𝐇𝐀 𝐊𝐎 𝐃𝐈𝐒𝐓𝐑𝐔𝐁 𝐍𝐀 🤧𝐊𝐑𝐎 𝐀𝐁𝐇𝐈 𝐒𝐇𝐄𝐕𝐈𝐍𝐆😜 𝐊𝐀𝐑𝐀 𝐑𝐇𝐈 𝐇𝐀𝐈
-        𝐘𝐀𝐊𝐄𝐄𝐍 𝐍À 𝐇𝐎 𝐓𝐎 😛𝐃𝐄𝐊𝐇 𝐋𝐎 😹💐✿`,attachment: fs.createReadStream(__dirname + `/noprefix/Isha.jpg`)
+        body: `${name}💐𝐀𝐋𝐄 𝐌𝐎𝐑𝐈 𝐌𝐀𝐈𝐘𝐀 𝐘𝐄𝐇 𝐈𝐒𝐇𝐀 𝐇𝐀𝐈 𝐌 𝐓𝐎 𝐃𝐀𝐑 𝐆𝐀𝐘𝐀💐✿`,attachment: fs.createReadStream(__dirname + `/noprefix/Isha.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
